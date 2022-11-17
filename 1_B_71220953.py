29fe9cd2d14284f5074fc34791ee0a6e69932176
McYeremia <117885019+McYeremia@users.noreply.github.com>
2022-11-17 12:54:17 +0700

    Add files via upload

---
+++ 1_B_71220953.py
@@ -0,0 +1,5 @@
+print ("========== ALAT PENGHITUNG LINGKARAN SENTOT ==========")
+phi = 3.14
+a = float(input ("Masukan Diameter (Meter) : "))
+
+print ("Jarak yang harus di tempuh : ", phi * a, "Meter")
---
+++ 2_B_71220953.py
@@ -0,0 +1,4 @@
+print ("========== INI PROGRAM SEDER HANA =========")
+print ("Masukan Angka yang ingin di Pangkatkan : ")
+a = int(input(">>"))
+print ("Angka anda telah di pangkat kan 7! anda mendapat angka : ", a ** 7)
