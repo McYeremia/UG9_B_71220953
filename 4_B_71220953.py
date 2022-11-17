6016417f6bab5fc8a4930338133160454f6d9d27
McYeremia <117885019+McYeremia@users.noreply.github.com>
2022-11-17 14:47:18 +0700

    Add files via upload

---
+++ 4_B_71220953.py
@@ -0,0 +1,54 @@
+nickname = input("STATE YOUR NAME: ")
+print ("=========================================================================")
+print (" ")
+print ("<<<<<<<<<< Adventure of", nickname, ">>>>>>>>>>")
+print (" ")
+print ("Welcome to the Adventure of", nickname, "!")
+print (" ")
+print ("Are You Male / Female? [M/F]")
+gender = input (">>")
+if gender == "M":
+    print
+else:
+    if gender == "F":
+        print
+     
+print (" ")
+print (" ")
+print (" ")
+print ("One day, A young adventurer named", nickname)
+print ("spawned in a peacefull Village of Orion")
+print (nickname, "walked out of Village and see a slime jumping around.")
+print (" ")
+print ("picked up a... (Sword/Bow)")
+a = input (">>")
+if a == "Sword":
+    print ("and attack the slime.")
+    print ("the slime get sliced and died. adventurer gain a level.")
+else:
+    if a == "Bow":
+        print ("and attack the slime.")
+        print ("the slime get shot and died. adventurer gain a level.")
+    else:
+        print ("Wrong Input")
+
+print (" ")
+print (nickname, "The Adventurer, then run towards a chest that glows from the inside")
+print ("PICK ONE!")
+print ("[OPEN/WALK]")
+chest = input (">>")
+print ("The Adventurer Finds a Gold and brings it Home to the Village.")
+print (" ")
+print ("as the adventure walks, he started to feel more and more tired.")
+print ("Take a [Break] / Keep [Walk]ing")
+input (">>")
+
+print (" ")
+print (nickname, "Arrived at the village and see RTX 3090 Ti on sale")
+print ("With the price of 1 gold")
+input ("[BUY] / [PASS]>>")
+
+print ("Congratulation! you got your dream stuff!")
+print ("Now you can play Minecraft with Raytracing FOREVER!")
+print (" ")
+print ("And live Happily ever After!~~")
